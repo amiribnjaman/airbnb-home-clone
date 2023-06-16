0c052a356fed2a08fedc147e13e20a266e1da1d8
amiribnjaman <amiribnjaman@gmail.com>
2023-06-16 21:40:20 +0600

    Sleep section done

---
+++ src/Components/Sleep.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Sleep() {
+    return (
+        <div>
+            <h3 className='font-semibold text-lg mb-3'>Where you'll sleep</h3>
+
+            {/* Sleep card */}
+            <div className='mt-2'>
+                <div className='border border-1 rounded-md px-5 py-4 w-1/4'>
+
+                    <h6 className='font-semibold text-[13px]'>Bedroom</h6>
+                    <p className='text-[11px] font-semibold'>1 Bed room</p>
+                </div>
+            </div>
+        </div>
+    )
+}
