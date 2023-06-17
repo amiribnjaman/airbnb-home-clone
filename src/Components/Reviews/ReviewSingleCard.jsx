01f0480523e0705ff1c4213ff6384150440ce315
amiribnjaman <amiribnjaman@gmail.com>
2023-06-17 18:08:34 +0600

    Comment section implemented

--- src/Components/Reviews/ReviewSingleCard.jsx
+++ src/Components/Reviews/ReviewSingleCard.jsx
@@ -1,5 +1,6 @@
 
 import React from 'react'
+import Img from '../../Assets/Images/host.png'
 
 export default function ReviewSingleCard({ review }) {
     const { id, name, image, comment, rating, date } = review
@@ -12,7 +13,7 @@ export default function ReviewSingleCard({ review }) {
     return (
         <div className=''>
             <div className='flex gap-2'>
-                <img className='shadow w-10 h-10 bg-gray-300 rounded-full' src={image} alt="" />
+                <img className='shadow w-10 h-10 bg-gray-300 rounded-full' src={Img} alt="" />
                 <div>
                     <h5 className='font-semibold'>{name}</h5>
                     <h6 className='text-[13px]'>{date}</h6>
@@ -22,7 +23,7 @@ export default function ReviewSingleCard({ review }) {
                 <p className='font-semibold text-[14px]'>
                     {newComment?.length > 0 ? <>
                         {newComment} <div>
-                            <button className='mt-3 underline'>Show more > </button>
+                            <button className='mt-3 underline'>Show more &#8250; </button>
                         </div>
                     </> : comment}
                 </p>
